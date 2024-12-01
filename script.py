8f496b3129a2c95ede76b03b172c2ba87c6c09be
ebonini <44753902+ebonini@users.noreply.github.com>
2024-12-01 18:29:28 -0300

    Update script.py

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 import os
 import json
 import requests
+from googletrans import Translator
 
 # Carregar as credenciais do Google Sheets dos segredos do GitHub Actions
 credentials_json = os.environ["GOOGLE_CREDENTIALS_JSON"]
@@ -14,6 +15,9 @@ client = gspread.authorize(credentials)
 
 # Chave de API da TMDb
 tmdb_api_key = os.environ["TMDB_API_KEY"]
+
+# Inicializar o tradutor
+translator = Translator()
 
 # Função para obter dados do filme da TMDb
 def get_movie_data(tvg_name):
@@ -47,14 +51,20 @@ with open(m3u_file_path, "w") as file:
         if movie_data:
             logo = f"https://image.tmdb.org/t/p/w600_and_h900_bestv2/{movie_data['poster_path']}" if movie_data['poster_path'] else ""
             description = movie_data["overview"] if movie_data["overview"] else ""
+            
+            # Traduzir a descrição para o português
+            if description:
+                translated_description = translator.translate(description, src='en', dest='pt').text
+            else:
+                translated_description = ""
         else:
             logo = ""
-            description = ""
+            translated_description = ""
 
         group_title = row["group-title"]
         nome_filme = row["Nome_Filme"]
         
-        m3u_line = f'#EXTINF:-1 tvg-type="movie" tvg-name="{tvg_name}" tvg-logo="{logo}" description="{description}" group-title="{group_title}", {nome_filme}\n'
+        m3u_line = f'#EXTINF:-1 tvg-type="movie" tvg-name="{tvg_name}" tvg-logo="{logo}" description="{translated_description}" group-title="{group_title}", {tvg_name}\n'
         file.write(m3u_line)
         print(f"Escrevendo linha: {m3u_line.strip()}")
 
