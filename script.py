a5ee273297c85b662fa7a1fdfe2c44a873e4ad53
ebonini <44753902+ebonini@users.noreply.github.com>
2024-12-01 17:10:10 -0300

    Update script.py

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 import json
+import requests
 
 # Carregar as credenciais do Google Sheets dos segredos do GitHub Actions
 credentials_json = os.environ["GOOGLE_CREDENTIALS_JSON"]
@@ -10,6 +11,19 @@ credentials_dict = json.loads(credentials_json)
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
 client = gspread.authorize(credentials)
+
+# Chave de API da TMDb
+tmdb_api_key = os.environ["TMDB_API_KEY"]
+
+# Função para obter dados do filme da TMDb
+def get_movie_data(tvg_name):
+    url = f"https://api.themoviedb.org/3/search/movie?api_key={tmdb_api_key}&query={tvg_name}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        results = response.json().get("results")
+        if results:
+            return results[0]  # Retorna o primeiro resultado
+    return None
 
 # Abrir a planilha
 spreadsheet = client.open("Nome_da_Sua_Planilha")
@@ -22,12 +36,15 @@ rows = worksheet.get_all_records()
 with open("playlist.m3u", "w") as file:
     for row in rows:
         tvg_name = row["tvg-name"]
-        logo = row["tvg-logo"]
-        description = row["description"]
-        group_title = row["group-title"]
-        nome_filme = row["Nome_Filme"]
         
-        m3u_line = f'#EXTINF:-1 tvg-type="movie" tvg-name="{tvg_name}" tvg-logo="{logo}" description="{description}" group-title="{group_title}", {nome_filme}\n'
-        file.write(m3u_line)
+        movie_data = get_movie_data(tvg_name)
+        if movie_data:
+            logo = f"https://image.tmdb.org/t/p/w600_and_h900_bestv2/{movie_data['poster_path']}" if movie_data['poster_path'] else ""
+            description = movie_data["overview"] if movie_data["overview"] else ""
+            group_title = row["group-title"]
+            nome_filme = row["Nome_Filme"]
+            
+            m3u_line = f'#EXTINF:-1 tvg-type="movie" tvg-name="{tvg_name}" tvg-logo="{logo}" description="{description}" group-title="{group_title}", {nome_filme}\n'
+            file.write(m3u_line)
 
 print("Lista M3U gerada com sucesso!")
